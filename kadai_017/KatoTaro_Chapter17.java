2732abc4d8722fb330ca83d8d97fb3710497ecd6
imado601 <145324974+imado601@users.noreply.github.com>
2024-03-24 12:31:41 +0900

    課題再々提出

---
+++ kadai_017/KatoTaro_Chapter17.java
@@ -0,0 +1,13 @@
+public class KatoTaro_Chapter17 extends Kato_Chapter17 {
+    public KatoTaro_Chapter17() {
+        setGivenName("太郎");
+    }
+
+    public void setGivenName(String name) {
+        this.givenName = name;
+    }
+
+    public void eachIntroduce() {
+        System.out.println("私はJavaが得意です");
+    }
+}
