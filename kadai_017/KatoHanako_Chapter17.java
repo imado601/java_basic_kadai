bfb768d438b5beac530e2b178ed97ac117a87740
imado601 <145324974+imado601@users.noreply.github.com>
2024-03-25 19:09:51 +0900

    Delete kadai_017 directory

--- kadai_017/KatoHanako_Chapter17.java
+++
@@ -1,9 +0,0 @@
-public class KatoHanako_Chapter17 extends Kato_Chapter17 {
-    public void setGivenName() {
-        this.givenName = "花子";
-    }
-
-    public void eachIntroduce() {
-        System.out.println("趣味は読書です");
-    }
-}
--- kadai_017/KatoTaro_Chapter17.java
+++
@@ -1,9 +0,0 @@
-public class KatoTaro_Chapter17 extends Kato_Chapter17 {
-    public void setGivenName() {
-        this.givenName = "太郎";
-    }
-
-    public void eachIntroduce() {
-        System.out.println("私はJavaが得意です");
-    }
-}
