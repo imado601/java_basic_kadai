3d32dcc493ad226e1a0351d0d395ad432be45dbd
imado601 <145324974+imado601@users.noreply.github.com>
2024-03-25 19:10:42 +0900

    課題提出

---
+++ kadai_017/KatoTaro_Chapter17.java
@@ -0,0 +1,9 @@
+public class KatoTaro_Chapter17 extends Kato_Chapter17 {
+    public void setGivenName() {
+        this.givenName = "太郎";
+    }
+
+    public void eachIntroduce() {
+        System.out.println("Javaが得意です");
+    }
+}
